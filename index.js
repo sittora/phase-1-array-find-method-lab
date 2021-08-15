3dd38953e741fa4f35ea2f781422652b19cf71ed
sittora <sittora@gmail.com>
2021-08-14 21:01:07 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 
+function superbowlWin(recordArray) {
+   const game = recordArray.find(record => record.result === "W");
+   
+    if (!game) {
+        return undefined;
+    } else {
+        return game.year; 
+    }
+}
